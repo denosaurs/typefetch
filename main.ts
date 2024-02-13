d5e2508a841ce6264fec008aae91db600b735ce2
Elias Sjögreen <elias@5monkeys.se>
2024-02-13 17:21:59 +0100

    fix: change JSON utility types import to URL import

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-import { parseArgs } from "jsr:@std/cli";
-import { resolve } from "jsr:@std/path";
+import { parseArgs } from "jsr:@std/cli@0.215";
+import { resolve } from "jsr:@std/path@0.215";
 
 import { ModuleDeclarationKind, Project } from "ts-morph";
 
-import manifest from "./deno.json" with { type: "json" };
-
 import { addModuleComment, addComponents, addPathsObject } from "./mod.ts";
+
+export * from "./mod.ts";
 
 if (import.meta.main) {
   const args = parseArgs(Deno.args, {
@@ -19,23 +19,25 @@ if (import.meta.main) {
     console.error("Expected a single OpenAPI specification to transform");
     Deno.exit(1);
   }
-  
+
   const input = resolve(args._[0]);
   const output = resolve(args.output);
-  
+
   const openapi = (await import(input, { with: { type: "json" } })).default;
-  const project = new Project({ tsConfigFilePath: args.config });
+  const project = new Project({ tsConfigFilePath: args.config as string | undefined });
   const source = project.createSourceFile(output, undefined, { overwrite: true });
+
+  addModuleComment(source, openapi.info);
+  source.addImportDeclaration({
+    isTypeOnly: true,
+    moduleSpecifier: "https://raw.githubusercontent.com/denosaurs/typefetch/main/types/json.ts",
+    namedImports: ["JSONString"],
+  });
+
   const global = source.addModule({
     hasDeclareKeyword: true,
     declarationKind: ModuleDeclarationKind.Global,
     name: "global",
-  });
-
-  source.addImportDeclaration({
-    isTypeOnly: true,
-    moduleSpecifier: `jsr:${manifest.name}@${manifest.version}/types/json`,
-    namedImports: ["JSONString"],
   });
 
   addModuleComment(source, openapi.info);
