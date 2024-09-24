cec9c4a1010303bfe1e5838f8ef66b0b16545210
Elias Sjögreen <elias@5monkeys.se>
2024-09-24 14:17:42 +0200

    feat: Publish to npm

---
+++ node/shims.ts
@@ -0,0 +1,30 @@
+// deno-lint-ignore-file no-explicit-any
+import process from "node:process";
+
+export function addEventListener(
+  type: "unload",
+  listener: () => any,
+  options?: never,
+): void {
+  if (type !== "unload") {
+    console.warn(
+      "The `addEventListener` shim does not support any event type other than `unload`",
+    );
+    return;
+  }
+  if (listener.length >= 1) {
+    console.warn(
+      "The `addEventListener` shim does not support any arguments passed to the listener",
+    );
+    return;
+  }
+  if (options !== undefined) {
+    console.warn("The `addEventListener` shim does not support options");
+    return;
+  }
+
+  process.on("exit", listener);
+  process.on("beforeExit", listener);
+  process.on("SIGINT", listener);
+  process.on("uncaughtException", listener);
+}
